36d42276298c2b4d7b89a7ed8a13e6e69896bfb3
SinJiaGong <markkong318@gmail.com>
2017-05-22 00:04:56 +0900

    during develop

--- server.php
+++ server.php
@@ -4,6 +4,6 @@ require_once 'SocketServerClient.php';
 require_once 'RSocketServer.php';
 require_once 'RSocketServerClient.php';
 
-$server = new RSocketServer("172.31.26.230",9001); // Binds to determined IP
+$server = new RSocketServer("127.0.0.1",9001); // Binds to determined IP
 $server->infinite_loop(); // starts the loop.
 
